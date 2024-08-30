ce259bb32ebec8c7fe2df9428a58a54a62517bdc
Om Prakash <omprakash.snps@gmail.com>
2024-08-30 20:46:51 +0530

    AddAdmin
    Created backend logic for adding new admin

--- Backend/controller/userController.js
+++ Backend/controller/userController.js
@@ -68,3 +68,33 @@ export const login = catchAsyncErrors(async (req, res, next) => {
     }
     generateToken(user, "Login Successfully!", 201, res);
 });
+
+export const addNewAdmin = catchAsyncErrors(async (req, res, next) => {
+    const { firstName, lastName, email, phone, nic, dob, gender, password } = req.body;
+    if (!firstName || !lastName || !email || !phone || !nic || !dob || !gender || !password) {
+        return next(new ErrorHandler("Please Fill Full Form!", 400));
+    }
+
+    const isRegistered = await User.findOne({ email });
+    if (isRegistered) {
+        return next(new ErrorHandler("Admin With This Email Already Exists!", 400));
+    }
+
+    const admin = await User.create({
+        firstName,
+        lastName,
+        email,
+        phone,
+        nic,
+        dob,
+        gender,
+        password,
+        role: "Admin",
+    });
+
+    res.status(200).json({
+        success: true,
+        message: "New Admin Registered",
+        admin,
+    });
+});
