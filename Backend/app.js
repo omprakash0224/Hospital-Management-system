3e3e1e92550c71a3b32d2bebfb14d63b523ba3d2
Om Prakash <omprakash.snps@gmail.com>
2024-09-01 15:13:37 +0530

    API for appointment
    created API to send and get appointments

--- Backend/app.js
+++ Backend/app.js
@@ -6,6 +6,7 @@ import fileUpload from "express-fileupload";
 import { dbConnect } from "./database/dbConnect.js";
 import messageRouter from "./router/messageRouter.js";
 import userRouter from "./router/userRouter.js";
+import appointmentRouter from "./router/appointmentRouter.js"
 import { errorMiddleware } from "./middleware/errorMiddleware.js";
 
 const app = express();
@@ -28,6 +29,7 @@ app.use(fileUpload({
 
 app.use("/api/v1/message", messageRouter);
 app.use("/api/v1/user", userRouter);
+app.use("/api/v1/appointment", appointmentRouter);
 
 dbConnect();
 
---
+++ Backend/router/appointmentRouter.js
@@ -0,0 +1,10 @@
+import express from "express"
+import { getAllAppointments, postAppointment } from "../controller/appointmentController.js";
+import { isAdminAuthenticated, isPatientAuthenticated } from "../middleware/auth.js"
+
+const router = express.Router();
+
+router.post("/post", isPatientAuthenticated, postAppointment);
+router.get("/getall", isAdminAuthenticated, getAllAppointments);
+
+export default router;
