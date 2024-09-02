6f9be44c3f1776194b63feb2903efa1acb05970b
Om Prakash <omprakash.snps@gmail.com>
2024-09-02 20:22:50 +0530

    API to update and delete appointment

--- Backend/controller/appointmentController.js
+++ Backend/controller/appointmentController.js
@@ -84,3 +84,34 @@ export const getAllAppointments = catchAsyncErrors(async (req, res, next) => {
         appointments,
     });
 });
+
+export const updateAppointmentStatus = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.params;
+    let appointment = await Appointment.findById(id);
+    if (!appointment) {
+        return next(new ErrorHandler("Appointment not found!", 404));
+    }
+    appointment = await Appointment.findByIdAndUpdate(id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    res.status(200).json({
+        success: true,
+        message: "Appointment Status Updated!",
+        appointment,
+    });
+});
+
+export const deleteAppointment = catchAsyncErrors(async (req, res, next) => {
+    const { id } = req.params;
+    const appointment = await Appointment.findById(id);
+    if (!appointment) {
+        return next(new ErrorHandler("Appointment Not Found!", 404));
+    }
+    await appointment.deleteOne();
+    res.status(200).json({
+        success: true,
+        message: "Appointment Deleted!",
+    });
+});
