6d947e1d1798d67c6e10f7416e9f524de0d7c660
Om Prakash <omprakash.snps@gmail.com>
2024-09-06 00:11:15 +0530

    Update App.jsx
    setting up react toastify

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -5,6 +5,8 @@ import AboutUs from '../pages/AboutUs'
 import Appointment from '../pages/Appointment'
 import Register from '../pages/Register'
 import Login from '../pages/Login'
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const App = () => {
   return (
@@ -17,6 +19,7 @@ const App = () => {
           <Route path='/register' element={<Register/>}/>
           <Route path='/login' element={<Login/>}/>
         </Routes>
+        <ToastContainer position="top-center"/>
       </Router>
     </>
   )
