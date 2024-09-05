c45917972c4f500624ad8f0f86fe06466003a1c3
Om Prakash <omprakash.snps@gmail.com>
2024-09-06 00:06:53 +0530

    Initializing Pages and set up routing

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
 import {BrowserRouter as Router, Routes, Route} from "react-router-dom"
+import Home from '../pages/Home'
+import AboutUs from '../pages/AboutUs'
+import Appointment from '../pages/Appointment'
+import Register from '../pages/Register'
+import Login from '../pages/Login'
 
 const App = () => {
   return (
     <>
       <Router>
         <Routes>
-          <Route/>
+          <Route path='/' element={<Home/>}/>
+          <Route path='/appointment' element={<Appointment/>}/>
+          <Route path='/about' element={<AboutUs/>}/>
+          <Route path='/register' element={<Register/>}/>
+          <Route path='/login' element={<Login/>}/>
         </Routes>
       </Router>
     </>
