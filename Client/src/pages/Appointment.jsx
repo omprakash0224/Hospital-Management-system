73bca88117b702b1e68f6d563382a6488b0473d1
Om Prakash <omprakash.snps@gmail.com>
2025-06-28 15:19:29 +0530

    Added appointment page

--- Client/src/pages/Appointment.jsx
+++ Client/src/pages/Appointment.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Hero from '../components/Hero';
+import AppointmentForm from '../components/AppointmentForm';
 
 const Appointment = () => {
   return (
-    <div>
-      
-    </div>
+    <>
+      <Hero
+        title={"Schedule Your Appointment | ZeeCare Medical Institute"}
+        imageUrl={"/signin.png"}
+      />
+      <AppointmentForm />
+    </>
   )
 }
 
